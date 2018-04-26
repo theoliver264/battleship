4157551f543dfa5b257fe22afca029f145430f34
2d <2d@iMac-de-iMac23.local>
2018-04-25 19:32:50 -0500

    xd

--- sketch.js
+++ sketch.js
@@ -1,8 +1,20 @@
 function setup(){
     createCanvas(400,400)
     background(000)
+    noFill()
+    stroke(255)
+    let x
+    let y
+    for(let i = 0; i < 10; i++){
+        x = 0;
+        y = 0
+        for(let j = 0; j < 10; i++){
+            rect(x,y,40)
+            x *= 40
+        }
+        y += 40
+    }
 }
 
 function draw(){
-
 }
